4c93f451c28c33deae64badcd1db33b9e48307b6
VadymMartinuk <90600927+Morrison01@users.noreply.github.com>
2024-11-12 17:59:36 +0200

    Update SpeakGoodBye.js

--- lab2/SpeakGoodBye.js
+++ lab2/SpeakGoodBye.js
@@ -1,6 +1,8 @@
-// Змінна speakWord не виходить у глобальну область видимості
-var speakWord = "Good Bye";
-
-function speakGoodbye(name) {
-    console.log(speakWord + " " + name);
-}
+// Використовуємо IIFE, щоб уникнути глобальної видимості для speakWord
+(function (window) {
+    var speakWord = "Good Bye";
+    var speakGoodbye = function (name) {
+        console.log(speakWord + " " + name);
+    };
+    window.speakGoodbye = speakGoodbye; // Додаємо функцію в глобальний об'єкт window
+})(window);
